324e752be0a24c3ae6a88871678cdcbace7750b4
Nathi <snkosinathi45@gmail.com>
2026-04-14 07:59:50 +0200

    use headless opencv

--- backend/myserver/settings.py
+++ backend/myserver/settings.py
@@ -21,7 +21,10 @@ load_dotenv(os.path.join(Path(__file__).resolve().parent.parent, '.env'))
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-duqoi6mg5@fypw3@hy9%0=50sao8t$eti&_mtqt^(o%tf1yq(^')
 DEBUG = os.getenv('DJANGO_DEBUG', 'True').lower() in ('1', 'true', 'yes')
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "wonderful-generosity-production.up.railway.app",
+    ".railway.app"
+]
 
 INSTALLED_APPS = [
     'corsheaders',
