765c82bb3619ab7dd37a56a8b277c874246c8d6a
Nathi <snkosinathi45@gmail.com>
2026-04-23 19:35:49 +0200

    Fix production settings: use settings_production on Railway and set DJANGO_DEBUG=False

--- backend/myserver/wsgi.py
+++ backend/myserver/wsgi.py
@@ -11,6 +11,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myserver.settings')
+# Use production settings if DJANGO_SETTINGS_MODULE is not already set
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    # On production (Railway), DEBUG should be False
+    debug = os.getenv('DJANGO_DEBUG', 'True').lower() in ('1', 'true', 'yes')
+    settings_module = 'myserver.settings' if debug else 'myserver.settings_production'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+else:
+    # If explicitly set, use that
+    pass
 
 application = get_wsgi_application()
